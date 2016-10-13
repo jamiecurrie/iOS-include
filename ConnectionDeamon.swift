c50129108247769a96d94a197bd4bd62751afac7
Jamie Currie <jamiecurrieaudio@gmail.com>
2016-10-13 12:20:48 +0100

    swift 3 init

--- ConnectionDeamon.swift
+++ ConnectionDeamon.swift
@@ -9,16 +9,16 @@
 import Foundation
 import SystemConfiguration
 
-public class ConnectionDeamon {
+open class ConnectionDeamon {
     
-    var loop: NSTimer!
+    var loop: Timer!
     var mInterval: Double = 1.0
     var connected = true
     let events = EventManager()
     
     func start () {
         print("😈 ConnectionDeamon Starting...")
-        loop = NSTimer.scheduledTimerWithTimeInterval(mInterval, target: self, selector: #selector(ConnectionDeamon.deamon), userInfo: nil, repeats: true)
+        loop = Timer.scheduledTimer(timeInterval: mInterval, target: self, selector: #selector(ConnectionDeamon.deamon), userInfo: nil, repeats: true)
     }
     
     func stop () {
@@ -26,7 +26,7 @@ public class ConnectionDeamon {
         loop.invalidate()
     }
     
-    @objc private func deamon() {
+    @objc func deamon() {
         if !ConnectionDeamon.isConnectedToNetwork() {
             if (connected) {
                 print("😈 CONNECTION_LOST")
@@ -42,13 +42,17 @@ public class ConnectionDeamon {
         }
     }
     
-    private class func isConnectedToNetwork() -> Bool {
+    class func isConnectedToNetwork() -> Bool {
         var zeroAddress = sockaddr_in()
-        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
         zeroAddress.sin_family = sa_family_t(AF_INET)
-        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
-            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
         }
+
         var flags = SCNetworkReachabilityFlags()
         if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
             return false
