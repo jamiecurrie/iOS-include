7385c063eaa9658a8fa6211eb80eadef4f99a58f
Jamie Currie <jamiecurrieaudio@gmail.com>
2016-10-13 12:40:37 +0100

    Revert "Revert "Merge branch 'swift-3'""
    This reverts commit daff2e3be49ddd547cdcc9e0b38759e7cb4a141d.

--- Extentions.swift
+++ Extentions.swift
@@ -8,60 +8,135 @@
 
 import UIKit
 
-extension NSDate {
+public extension UIDevice {
     
-    func numberOfDaysUntilDateTime(toDateTime: NSDate, inTimeZone timeZone: NSTimeZone? = nil) -> Int {
-        let calendar = NSCalendar.currentCalendar()
-        if let timeZone = timeZone {
-            calendar.timeZone = timeZone
+    var modelName: String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8 , value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
         }
         
-        var fromDate: NSDate?, toDate: NSDate?
-        
-        calendar.rangeOfUnit(.Day, startDate: &fromDate, interval: nil, forDate: self)
-        calendar.rangeOfUnit(.Day, startDate: &toDate, interval: nil, forDate: toDateTime)
-        
-        let difference = calendar.components(.Day, fromDate: fromDate!, toDate: toDate!, options: [])
-        return difference.day
+        switch identifier {
+        case "iPod5,1":                                 return "iPod Touch 5"
+        case "iPod7,1":                                 return "iPod Touch 6"
+        case "iPhone3,1", "iPhone3,2", "iPhone3,3":     return "iPhone 4"
+        case "iPhone4,1":                               return "iPhone 4s"
+        case "iPhone5,1", "iPhone5,2":                  return "iPhone 5"
+        case "iPhone5,3", "iPhone5,4":                  return "iPhone 5c"
+        case "iPhone6,1", "iPhone6,2":                  return "iPhone 5s"
+        case "iPhone7,2":                               return "iPhone 6"
+        case "iPhone7,1":                               return "iPhone 6 Plus"
+        case "iPhone8,1":                               return "iPhone 6s"
+        case "iPhone8,2":                               return "iPhone 6s Plus"
+        case "iPhone9,1", "iPhone9,3":                  return "iPhone 7"
+        case "iPhone9,2", "iPhone9,4":                  return "iPhone 7 Plus"
+        case "iPhone8,4":                               return "iPhone SE"
+        case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":return "iPad 2"
+        case "iPad3,1", "iPad3,2", "iPad3,3":           return "iPad 3"
+        case "iPad3,4", "iPad3,5", "iPad3,6":           return "iPad 4"
+        case "iPad4,1", "iPad4,2", "iPad4,3":           return "iPad Air"
+        case "iPad5,3", "iPad5,4":                      return "iPad Air 2"
+        case "iPad2,5", "iPad2,6", "iPad2,7":           return "iPad Mini"
+        case "iPad4,4", "iPad4,5", "iPad4,6":           return "iPad Mini 2"
+        case "iPad4,7", "iPad4,8", "iPad4,9":           return "iPad Mini 3"
+        case "iPad5,1", "iPad5,2":                      return "iPad Mini 4"
+        case "iPad6,3", "iPad6,4", "iPad6,7", "iPad6,8":return "iPad Pro"
+        case "AppleTV5,3":                              return "Apple TV"
+        case "i386", "x86_64":                          return "Simulator"
+        default:                                        return identifier
+        }
     }
     
+}
+
+extension UILabel{
+    
+    func autoAdjustForTextHeight() {
+        
+        // create new label to get height
+        let label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: CGFloat.greatestFiniteMagnitude))
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.font = self.font
+        label.text = self.text
+        label.sizeToFit()
+        
+        self.sizeThatFits(CGSize(width: self.frame.width, height: label.frame.height))
+    }
+}
+
+extension URLRequest {
+    
+    func getQueryDictionary() -> NSDictionary {
+        
+        let params = NSMutableDictionary()
+        
+        if let query = (self.url?.query) {
+            let items = query.components(separatedBy: "&")
+            
+            for item in items {
+                let elts = item.components(separatedBy: "=")
+                params.setValue(elts.last!, forKey: elts.first!)
+            }
+        }
+        return params
+    }
+}
+
+extension Date {
+    
+    func numberOfDaysUntilDateTime(toDateTime: Date) -> Int {
+        
+        let calendar = Calendar.current
+        
+        let fromDate = calendar.startOfDay(for: self)
+        let toDate = calendar.startOfDay(for: toDateTime)
+        
+        let difference = calendar.dateComponents([.day], from: fromDate, to: toDate)
+        return difference.day!
+    }
+    
+    
     // this is to get round the int32 behavior in legency device in fucking swift... i love you really swift
-    func stupidDateToStringFix(dateString: String) -> NSDate {
+    func stupidDateToStringFix(_ dateString: String) -> Date {
         
-        let comps = NSDateComponents()
+        var comps = DateComponents()
         
-        let splitDate = dateString.componentsSeparatedByString(" ")[0]
+        let splitDate = dateString.components(separatedBy: " ")[0]
         
-        comps.year = Int(splitDate.componentsSeparatedByString("-")[0])!
-        comps.month = Int(splitDate.componentsSeparatedByString("-")[1])!
-        comps.day = Int(splitDate.componentsSeparatedByString("-")[2])!
+        comps.year = Int(splitDate.components(separatedBy: "-")[0])!
+        comps.month = Int(splitDate.components(separatedBy: "-")[1])!
+        comps.day = Int(splitDate.components(separatedBy: "-")[2])!
         
         
-        let splitTime = dateString.componentsSeparatedByString(" ")[1]
+        let splitTime = dateString.components(separatedBy: " ")[1]
         
-        comps.hour = Int(splitTime.componentsSeparatedByString(":")[0])!
-        comps.minute = Int(splitTime.componentsSeparatedByString(":")[1])!
-        comps.second = Int(splitTime.componentsSeparatedByString(":")[2])!
+        comps.hour = Int(splitTime.components(separatedBy: ":")[0])!
+        comps.minute = Int(splitTime.components(separatedBy: ":")[1])!
+        comps.second = Int(splitTime.components(separatedBy: ":")[2])!
         
-        let cal = NSCalendar.currentCalendar()
-        cal.timeZone = NSTimeZone(abbreviation: "UTC")!
+        var cal = Calendar.current
+        cal.timeZone = TimeZone(abbreviation: "UTC")!
         
-        if let date: NSDate = cal.dateFromComponents(comps) {
+        if let date: Date = cal.date(from: comps) {
             return date
         } else {
             print("❗️ Couldn't covert to NSDate: \(dateString)")
-            ErrorLogger().log(self.dynamicType, funcRef: #function, errorRef: "Couldn't covert to NSDate: \(dateString)")
-            return NSDate()
+            ErrorLogger().log(type(of: self) as! AnyClass, funcRef: #function, errorRef: "Couldn't covert to NSDate: \(dateString)")
+            return Date()
         }
     }
     
     func systemTimeZone() -> String {
         
-        let formattor = NSDateFormatter()
+        let formattor = DateFormatter()
         formattor.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        formattor.timeZone = NSTimeZone.systemTimeZone()
+        formattor.timeZone = TimeZone.current
         
-        return formattor.stringFromDate(self)
+        return formattor.string(from: self)
     }
 }
 
@@ -80,11 +155,11 @@ extension UIColor {
 }
 <<<<<<< HEAD
 
-extension NSData {
+extension Data {
     
     
     func getDictionaryFromJsonData () -> NSDictionary {
-        if let parsedObject: AnyObject? = try? NSJSONSerialization.JSONObjectWithData(self, options: []) {
+        if let parsedObject: AnyObject? = try! JSONSerialization.jsonObject(with: self, options: []) as AnyObject?? {
             if let dictionary = parsedObject as? NSDictionary {
                 
                 return dictionary
